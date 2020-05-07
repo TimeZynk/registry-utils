c6b634932848e3fc8490f1bb2f4e3b4056e49236
Johan Heander <johan@timezynk.com>
2020-05-07 15:03:19 +0200

    Switch back default export to memoized implementation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { cacheFactory } from './cacheFactory';
-import { dataBuilderFactory } from './dataBuilderFactory';
+import { memoizedDataBuilderFactory } from './dataBuilderFactory';
 import * as registryDefaultData from './registryDefaultData';
 import { defaultRegisters } from './defaultRegisters';
 
-export { cacheFactory, dataBuilderFactory, registryDefaultData, defaultRegisters };
+export { cacheFactory, memoizedDataBuilderFactory as dataBuilderFactory, registryDefaultData, defaultRegisters };
 
-export default dataBuilderFactory;
+export default memoizedDataBuilderFactory;
