1fb19d70da932fd0317e32e3458ce3f07ed214a1
Ester Daniel Ytterbrink <esterdaniel.ytterbrink@eficode.com>
2020-12-09 12:22:38 +0100

    Notes for next time

--- src/salary.test.ts
+++ src/salary.test.ts
@@ -2,22 +2,29 @@ import Immutable from 'immutable';
 import { stopCache } from './cacheFactory';
 import { dataBuilderFactory, DataBuilder } from './dataBuilderFactory';
 
-describe('Multiple salary lists for users', () => {
+describe('Custom value', () => {
     afterEach(() => {
         stopCache();
     });
 
-    //TODO:
-    // Use type aliases
-    // Use Immutable.fromJS
-
     // Notes:
     // -  don't need registry SALARY_LIST but we need reference to registry (we could just fake 'registry-id')
     // -  reference should be in regData
-    // -  we need a user registry which will contain 'hourly-salary' should have a standart value
+    // -  we need a user registry which will contain 'hourly-salary'
     // -  get custom 'hourly-salary' value as result
-    // - salary list ref should have a default value to fail the test
+    // - salary list ref should have a default value
     // - the different salary list items need distinct values for salary
+
+    // Things to try to find the bug:
+    // - Debug the test that pass in the debugger
+    // - Debug the actual code from the ui
+    // - More complex test date — copy from browser?
+    // - memoized
+    // - global state - cache
+    // - call functions twice
+    // - write a test for tzcontrol
+    // - write other tests for inspiration
+    // - weight order of the field
 
     let dataBuilder: DataBuilder;
     type FieldId = string;
@@ -26,55 +33,113 @@ describe('Multiple salary lists for users', () => {
     type RegDataId = string;
     type RegData = Immutable.Map<string, any>;
     type RegDataIndex = Immutable.Map<RegDataId, RegData>;
-
-    const fields: FieldIndex = Immutable.fromJS({
-        SALARY_LIST_REF: {
-            id: 'SALARY_LIST_REF',
-            'registry-id': 'USERS',
-            title: 'HOURLY_SALARY_REF',
-            'field-type': 'registry-reference',
-            settings: {
+    it('should override default value', () => {
+        const fields: FieldIndex = Immutable.fromJS({
+            SALARY_LIST_REF: {
+                id: 'SALARY_LIST_REF',
+                'registry-id': 'USERS',
+                title: 'HOURLY_SALARY_REF',
+                'field-type': 'registry-reference',
+                settings: {
+                    'registry-id': 'SALARY_LIST',
+                },
+                values: {
+                    'default-val': 'SALARY_LIST_DEFAULT',
+                },
+            },
+            HOURLY_SALARY: {
+                id: 'HOURLY_SALARY',
                 'registry-id': 'SALARY_LIST',
+                'field-type': 'number',
+                'field-section': 'generic',
             },
-            values: {
-                'default-val': 'SALARY_LIST_1',
+        });
+        const regData: RegDataIndex = Immutable.fromJS({
+            SALARY_LIST_DEFAULT: {
+                id: 'SALARY_LIST_DEFAULT',
+                'registry-id': 'SALARY_LIST',
+                values: {
+                    HOURLY_SALARY: 1.1,
+                },
             },
-        },
-        HOURLY_SALARY: {
-            id: 'HOURLY_SALARY',
-            'registry-id': 'SALARY_LIST',
-            'field-type': 'number',
-            'field-section': 'generic',
-        },
-    });
-    const regData: RegDataIndex = Immutable.fromJS({
-        SALARY_LIST_1: {
-            id: 'SALARY_LIST_1',
-            'registry-id': 'SALARY_LIST',
-            values: {
-                HOURLY_SALARY: 1.1,
+            SALARY_LIST_CUSTOM: {
+                id: 'SALARY_LIST_CUSTOM',
+                'registry-id': 'SALARY_LIST',
+                values: {
+                    HOURLY_SALARY: 1.2,
+                },
             },
-        },
-        SALARY_LIST_2: {
-            id: 'SALARY_LIST_2',
-            'registry-id': 'SALARY_LIST',
-            values: {
-                HOURLY_SALARY: 1.2,
-            },
-        },
-    });
+        });
 
-    const users: RegDataIndex = Immutable.fromJS({
-        USER_1: {
-            id: 'USER_1',
-            'registry-id': 'USERS',
-            values: {
-                SALARY_LIST_REF: 'SALARY_LIST_2',
+        const users: RegDataIndex = Immutable.fromJS({
+            USER_1: {
+                id: 'USER_1',
+                'registry-id': 'USERS',
+                values: {
+                    SALARY_LIST_REF: 'SALARY_LIST_CUSTOM',
+                },
             },
-        },
-    });
+        });
 
-    it('refData should have a proper referenced value without overriden set value by default-val', () => {
+        dataBuilder = dataBuilderFactory(fields, regData, users);
+        const timeReport: RegData = Immutable.Map({
+            id: 'REPORT_1',
+            start: '',
+            end: '',
+            'registry-id': 'REPORTS',
+            'user-id': 'USER_1',
+        });
+        const refData = dataBuilder(timeReport);
+        expect(refData?.get('HOURLY_SALARY')).toEqual(1.2);
+    });
+    it('should override default value for complex case', () => {
+        const fields: FieldIndex = Immutable.fromJS({
+            SALARY_LIST_REF: {
+                id: 'SALARY_LIST_REF',
+                'registry-id': 'USERS',
+                title: 'HOURLY_SALARY_REF',
+                'field-type': 'registry-reference',
+                settings: {
+                    'registry-id': 'SALARY_LIST',
+                },
+                values: {
+                    'default-val': 'SALARY_LIST_DEFAULT',
+                },
+            },
+            HOURLY_SALARY: {
+                id: 'HOURLY_SALARY',
+                'registry-id': 'SALARY_LIST',
+                'field-type': 'number',
+                'field-section': 'generic',
+            },
+        });
+        const regData: RegDataIndex = Immutable.fromJS({
+            SALARY_LIST_DEFAULT: {
+                id: 'SALARY_LIST_DEFAULT',
+                'registry-id': 'SALARY_LIST',
+                values: {
+                    HOURLY_SALARY: 1.1,
+                },
+            },
+            SALARY_LIST_CUSTOM: {
+                id: 'SALARY_LIST_CUSTOM',
+                'registry-id': 'SALARY_LIST',
+                values: {
+                    HOURLY_SALARY: 1.2,
+                },
+            },
+        });
+
+        const users: RegDataIndex = Immutable.fromJS({
+            USER_1: {
+                id: 'USER_1',
+                'registry-id': 'USERS',
+                values: {
+                    SALARY_LIST_REF: 'SALARY_LIST_CUSTOM',
+                },
+            },
+        });
+
         dataBuilder = dataBuilderFactory(fields, regData, users);
         const timeReport: RegData = Immutable.Map({
             id: 'REPORT_1',
